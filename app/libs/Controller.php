b7669ccfe5199d06ccd8de6b4eb1c7b078702039
arkovask <48015320+arkovask@users.noreply.github.com>
2020-05-31 17:06:24 +0300

    Add files via upload

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -1,25 +1,23 @@
 <?php
-
 /**
- *
  * Class Controller
  * Loads views and models
  */
+
 class Controller
 {
-  // load view
-  public function view($viewFile, $data = array()){
-    if(file_exists('../app/views/'.$viewFile.'.php')){
-      require_once '../app/views/'.$viewFile.'.php';
-    } else {
-      die($viewFile.'.php does not exist');
+    // Load View
+    public function view($viewFile, $data = array()){
+        if(file_exists('../app/views/'.$viewFile.'.php')){
+            require_once '../app/views/'.$viewFile.'.php';
+        } else {
+            die($viewFile.'.php does not exist');
+        }
     }
-  }
-
-  // load model
-  public function model($modelFile){
-    require_once '../app/models/'.$modelFile.'.php';
-    $model = new $modelFile();
-    return $model;
-  }
+    // Load model
+    public function model($modelFile){
+        require_once '../app/models/' .$modelFile.'.php';
+        $model = new $modelFile();
+        return $model;
+    }
 }
